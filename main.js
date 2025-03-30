925514a0174d625a31fe6cc8a284b504619fa858
YunQiAI <excerpt.10.able@icloud.com>
2025-03-30 15:50:48 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow, globalShortcut, ipcMain } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.loadFile('index.html');
+
+  globalShortcut.register('CommandOrControl+Shift+H', () => {
+    win.webContents.send('trigger-analysis');
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('will-quit', () => {
+  globalShortcut.unregisterAll();
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
